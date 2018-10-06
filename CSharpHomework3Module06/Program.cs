36006fe94a7296911f9f16b4855e590d4b1e9141
NajaNaja <NajaNaja89@gmail.com>
2018-10-06 13:16:54 +0600

    first commit

---
+++ CSharpHomework3Module06/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CSharpHomework3Module06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleLibrary.Person p1 = new ConsoleLibrary.Person();
+            Console.WriteLine("Enter Person Name");
+            p1.SetName(Console.ReadLine());
+            Console.WriteLine("Enter Person SerName");
+            p1.SetSerName(Console.ReadLine());
+
+            Console.WriteLine(ConsoleLibrary.Person.Person_(p1));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleLibrary/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleLibrary
+{
+    public class Person
+    {
+        private string name;
+        private string sername;
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        public void SetSerName(string sername)
+        {
+            this.sername = sername;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public string GetSerName()
+        {
+            return sername;
+        }
+
+        static public string Person_(Person p1)
+        {
+            string Person = "Person name is " + p1.GetName() + " person ser name is " + p1.GetSerName();
+            return Person;
+        }
+    }
+}
