59ea86276c6675fc71921b3172d8696108b46ec4
NajaNaja <NajaNaja89@gmail.com>
2018-10-07 13:16:04 +0600

    task2 done

--- CSharpHomework3Module06/Program.cs
+++ CSharpHomework3Module06/Program.cs
@@ -11,13 +11,28 @@ namespace CSharpHomework3Module06
     {
         static void Main(string[] args)
         {
-            ConsoleLibrary.Person p1 = new ConsoleLibrary.Person();
-            Console.WriteLine("Enter Person Name");
-            p1.SetName(Console.ReadLine());
-            Console.WriteLine("Enter Person SerName");
-            p1.SetSerName(Console.ReadLine());
+            string menu = "";
+            Console.WriteLine("Press 1 to task1");
+            Console.WriteLine("Press 2 to task2");
 
-            Console.WriteLine(ConsoleLibrary.Person.Person_(p1));
+            menu = Console.ReadLine();
+            if (menu == "1")
+            {
+                ConsoleLibrary.Person p1 = new ConsoleLibrary.Person();
+                Console.WriteLine("Enter Person Name");
+                p1.SetName(Console.ReadLine());
+                Console.WriteLine("Enter Person SerName");
+                p1.SetSerName(Console.ReadLine());
+
+                Console.WriteLine(ConsoleLibrary.Person.Person_(p1));
+            }
+
+            else if (menu == "2")
+            {
+                ConsoleLibrary.StringNumbers sn1 = new ConsoleLibrary.StringNumbers();
+                Console.WriteLine(sn1.GetSTRING() + " " + sn1.GetINT());
+                
+            }
             Console.ReadLine();
         }
     }
---
+++ ConsoleLibrary/StringNumbers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleLibrary
+{
+    public class StringNumbers
+    {
+        private const string STRING = "the answer to life the universe and everything is";
+        private const int INT = 42;
+
+        public string GetSTRING()
+        {
+            return STRING;
+        }
+
+        public int GetINT()
+        {
+            return INT;
+        }
+
+    }
+}
